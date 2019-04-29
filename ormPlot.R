562bc419cca784aa562d8243998bcfc06dcd6eb5
Richard Meitern <richard.meitern@gmail.com>
2019-04-29 13:46:05 +0300

    function to filter out only odds ratio info

--- ormPlot.R
+++ ormPlot.R
@@ -1,3 +1,23 @@
 forestplot<-function(summary_object){
   print("not Implemented")
 }
+
+
+orm.oddstable <- function(x, ..., type="plain", digits = 5, table.env=FALSE)
+{
+  switch(type,
+         latex = rms:::latex.summary.rms(x, ..., file='', table.env=table.env),
+         html  = return(rms:::html.summary.rms(x, ...)),
+         plain = {
+           rows <- nrow(x)%/%2
+           cstats <- dimnames(x)[[1]][seq(1,rows,2)]
+           for(i in 1 : 7) {
+             cstats <- cbind(cstats, format(signif(x[c(FALSE, TRUE), i], digits)))
+             }
+           dimnames(cstats) <- list(rep("", nrow(cstats)), 
+                                    c("Factor", dimnames(x)[[2]][1 : 7]))
+           return(cstats)
+         }
+  )
+  invisible()
+}

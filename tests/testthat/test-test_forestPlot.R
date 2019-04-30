48e4bdaaa2cab95ce54d2453de15dd334d611ea5
Richard Meitern <rix133@ut.ee>
2019-04-30 15:36:10 +0300

    added test for making plot

---
+++ tests/testthat/test-test_forestPlot.R
@@ -0,0 +1,11 @@
+##  Proto library for accessing sub-components of the ggplot2 plot objects
+library(proto)
+
+#READ THE TEST DATA
+test_data_001<-readRDS("../testdata/test_data_001.rds")
+
+test_that("Plotting actually works",{
+  p <- orm.forestplot(test_data_001)
+  expect_true(ggplot2::is.ggplot(p))
+  expect_error(print(p), NA)
+})

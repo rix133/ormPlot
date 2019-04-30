e5a99425aec85cacdfdf035e326f841324dd6d6f
Richard Meitern <rix133@ut.ee>
2019-04-30 18:21:20 +0300

    initial plot table and graph using ggplot2

--- R/ormPlot.R
+++ R/ormPlot.R
@@ -1,5 +1,12 @@
+
 orm.forestplot<-function(summary_object){
-  print("not Implemented")
+  oddstable <- orm.oddstable(summary_object)
+  tableplot <- plot.oddstable(oddstable)
+
+
+  return(tableplot)
+
+
 }
 
 
@@ -22,3 +29,67 @@ orm.oddstable <- function(x, ..., type="plain", digits = 5, table.env=FALSE)
          }
   )
 }
+
+plot.oddstable<-function(x, digits = 3){
+  x<-as.data.frame(x)
+  lables = colnames(x)
+  tableplot<-ggplot2::ggplot(x,ggplot2::aes(y=rownames(x)))+
+    ggplot2::geom_text(ggplot2::aes(x=lables[4], label=round(x[,4],digits))) +
+    ggplot2::geom_text(ggplot2::aes(x=lables[6], label=round(x[,6], digits))) +
+    ggplot2::geom_text(ggplot2::aes(x=lables[7], label=round(x[,7], digits))) +
+    ggplot2::scale_x_discrete(position="top", name = NULL) +
+    ggplot2::scale_y_discrete(limits= rev(rownames(x))) +
+    ggplot2::theme(axis.ticks = ggplot2::element_blank(),
+                   axis.title.y = ggplot2::element_blank(),
+                   axis.line.y = ggplot2::element_blank(),
+                   axis.text = ggplot2::theme_get()$text)
+
+
+  invisible(tableplot)
+
+}
+
+plot.orm.graph<-function(x, custom.row.names = c(), ylab = "Odds ratio (95% CI)"){
+    #set the theme
+    ggplot2::theme_set(ggplot2::theme_bw())
+    #set some other theme options#
+    ggplot2::theme_update(
+    axis.line = ggplot2::element_line(colour = "black"),
+    panel.grid.major = ggplot2::element_blank(),
+    panel.grid.minor = ggplot2::element_blank(),
+    panel.border = ggplot2::element_blank(),
+    panel.background = ggplot2::element_blank(),
+    axis.text.y = ggplot2::element_blank(),
+    axis.ticks.y = ggplot2::element_blank(),
+    axis.title.y= ggplot2::element_blank(),
+    text = ggplot2::element_text(size = 12),
+    plot.margin = grid::unit(c(0,0,0,0), "lines")
+  )
+    x$lables <-rownames(x)
+    print("70")
+    if(length(custom.row.names) == nrow(x)){
+      x$lables <- custom.row.names
+    }
+
+    p<-ggplot2::ggplot(x, aes(x=lables,y=Effect, ymin=`Lower 0.95`, ymax= `Upper 0.95`))+
+      ggplot2::geom_pointrange() +
+
+      # add a dotted line at x=1 after flip
+      ggplot2::geom_hline(yintercept=1, lty=2) +
+
+      # flip coordinates (puts labels on y axis)
+      ggplot2::coord_flip() +
+      ggplot2::ylab(ylab) +
+
+      ggplot2::scale_y_continuous(breaks = c(0.5,1,1.5,2,3,4)) +
+      # set the y lables sam for plot and table
+      ggplot2::scale_x_discrete(limits = rev(tabletext[,1])) +
+
+      # use the theme set prevoiosly
+      ggplot2::theme()
+
+    invisible(p)
+}
+
+
+

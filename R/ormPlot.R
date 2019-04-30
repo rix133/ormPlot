ea9d00d80b8c284cd2b50c20f3d69833f4565317
Richard Meitern <rix133@ut.ee>
2019-04-30 12:45:30 +0300

    fixed code not to produce a warning

--- R/ormPlot.R
+++ R/ormPlot.R
@@ -9,15 +9,9 @@ orm.oddstable <- function(x, ..., type="plain", digits = 5, table.env=FALSE)
          latex = rms:::latex.summary.rms(x, ..., file='', table.env=table.env),
          html  = return(rms:::html.summary.rms(x, ...)),
          plain = {
-           rows <- nrow(x)%/%2
-           cstats <- dimnames(x)[[1]][seq(1,rows,2)]
-           for(i in 1 : 7) {
-             cstats <- cbind(cstats, format(signif(x[c(FALSE, TRUE), i], digits)))
-             }
-           dimnames(cstats) <- list(rep("", nrow(cstats)), 
-                                    c("Factor", dimnames(x)[[2]][1 : 7]))
-           return(cstats)
+           cstats <- x[c(FALSE, TRUE),c(1:7)]
+           dimnames(cstats) <- list(dimnames(x[c(TRUE,FALSE),])[[1]], dimnames(x)[[2]][1 : 7])
+           invisible(cstats)
          }
   )
-  invisible()
 }

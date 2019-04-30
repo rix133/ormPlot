21bd2445f62047bcb3455015000d6db9ce7d93d9
Richard Meitern <rix133@ut.ee>
2019-04-30 12:44:58 +0300

    added first tests

--- tests/testthat/test-summary_to_table.R
+++ tests/testthat/test-summary_to_table.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+#READ THE TEST DATA
+test_data_001<-readRDS("../testdata/test_data_001.rds")
+
+test_that("html table is produced using html type", {
+  table<-orm.oddstable(test_data_001,type = "html")
+  expect_s3_class(table,c('html','character'))
+
+})
+
+test_that("matrix is produced witho plain type",{
+  expect_that(orm.oddstable(test_data_001,type = "plain"), is_a("matrix"))
+
+})
+
+test_that("matrix has the correct number of rows",{
+  expect_that(nrow(orm.oddstable(test_data_001)), equals(9))
+
 })

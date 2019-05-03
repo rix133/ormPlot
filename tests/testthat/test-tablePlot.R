7530464af1a174e5ddb3bbaa51787504dfa3cb25
Richard Meitern <rix133@ut.ee>
2019-05-03 17:16:12 +0300

    function creates a ggplot object

--- tests/testthat/test-tablePlot.R
+++ tests/testthat/test-tablePlot.R
@@ -1,3 +1,12 @@
+##  Proto library for accessing sub-components of the ggplot2 plot objects
+library(proto)
+
+#READ THE TEST DATA
+test_data_001<-readRDS("../testdata/test_data_001.rds")
+
+
 test_that("table plotting works", {
-  succeed()
+  p <- plot.oddstable(test_data_001)
+  expect_true(ggplot2::is.ggplot(p))
+  expect_error(print(p), NA)
 })

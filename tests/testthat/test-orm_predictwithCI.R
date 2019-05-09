ef0c94b7b3d193d2feafc85a8e80e7dc89f4152f
Richard Meitern <rix133@ut.ee>
2019-05-09 12:02:22 +0300

    added a rms compatibilty check test

--- tests/testthat/test-orm_predictwithCI.R
+++ tests/testthat/test-orm_predictwithCI.R
@@ -5,3 +5,19 @@ test_that("result is a data frame", {
   p <- orm.predictwithCI(test_model_001, cran_rzs, Rural, max_SEP_3, sex)
   expect_true(is.data.frame(p))
 })
+
+test_that("result has expected prediction data in it", {
+  n = 10
+  df <- orm.predictwithCI(test_model_001, cran_rzs, Rural, max_SEP_3, sex, np = n)
+
+  ocran_pred_frame<-rms::Predict(test_model_001, cran_rzs, Rural, max_SEP_3, sex, type = "model.frame",np=n)
+
+  #generate the probabilties for each level of education using a built in function that deos not give CI's
+  df_fitted<-rms:::predict.orm(test_model_001,ocran_pred_frame, type="fitted.ind")
+
+
+  for(i in 1:length(df_fitted)){
+    expect_equal(0, (df$Propability[i]-df_fitted[i]), tolerance = 0.00000001)
+  }
+
+})

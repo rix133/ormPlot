64df6340073cb7e390f067b24b2df7c668af3028
Richard Meitern <rix133@ut.ee>
2019-05-09 14:28:38 +0300

    reformated apparence

--- tests/testthat/test-plot_orm_graph.R
+++ tests/testthat/test-plot_orm_graph.R
@@ -1,5 +1,5 @@
 #READ THE TEST DATA
-test_data_001<-readRDS("../testdata/test_data_001.rds")
+test_data_001 <- readRDS("../testdata/test_data_001.rds")
 
 test_that("returns a ggplot object", {
   p <- plot.orm.graph(test_data_001)

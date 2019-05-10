7231f556439c12d5d50677cf82db48042ad98439
Richard Meitern <rix133@ut.ee>
2019-05-10 16:43:56 +0300

    eliminating problems that lead to build warnings

---
+++ tests/testthat/test-summary_orm.R
@@ -0,0 +1,8 @@
+#READ THE TEST DATA
+test_model_001<-readRDS("../testdata/test_model_001.rds")
+
+
+test_that("object has the right class", {
+  a<-summary(test_model_001)
+  testthat::expect_s3_class(a,"summary.orm")
+})

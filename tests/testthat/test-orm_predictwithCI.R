3d486fb8c1b7cdf6998d1cdbbcd2359daa268b25
Richard Meitern <rix133@ut.ee>
2019-06-14 14:46:42 +0300

    renamed to match new function name added a test model 3

--- tests/testthat/test-orm_predictwithCI.R
+++ tests/testthat/test-orm_predictwithCI.R
@@ -13,16 +13,21 @@ test_model_001<-rms::orm(educ_3 ~ Rural + sex + max_SEP_3 + n_siblings +
 test_model_002<-rms::orm(educ_3 ~ Rural + sex + n_siblings + cran_rzs + height_rzs +
                       FW_rzs + YOBc + (YOBc * sex) + (YOBc * Rural), data = test_data)
 
+test_model_003 <- rms::orm(educ_3 ~ Rural + sex + max_SEP_3 + cran_rzs, data = test_data)
+
+
 test_that("result is a data frame", {
-  p <- predict_with_ci.orm(test_model_001, cran_rzs, Rural, max_SEP_3, sex)
+  p <- predict_with_ci(test_model_001, cran_rzs, Rural, max_SEP_3, sex)
   expect_true(is.data.frame(p))
-  p <- predict_with_ci.orm(test_model_002, cran_rzs, Rural, sex)
+  p <- predict_with_ci(test_model_002, cran_rzs, Rural, sex)
   expect_true(is.data.frame(p))
+  p3 <- predict_with_ci(test_model_003, cran_rzs)
+  expect_true(is.data.frame(p3))
 })
 
 test_that("result has expected prediction data in it", {
   n = 10
-  df <- predict_with_ci.orm(test_model_001, cran_rzs, Rural, max_SEP_3, sex, np = n)
+  df <- predict_with_ci(test_model_001, cran_rzs, Rural, max_SEP_3, sex, np = n)
 
   ocran_pred_frame<-rms::Predict(test_model_001, cran_rzs, Rural, max_SEP_3, sex, type = "model.frame",np=n)
 
@@ -37,13 +42,15 @@ test_that("result has expected prediction data in it", {
 })
 
 test_that("result has expected column names", {
-  p <- predict_with_ci.orm(test_model_001, cran_rzs, Rural, sex)
+  p <- predict_with_ci(test_model_001, cran_rzs, Rural, sex)
   pcols<- colnames(p[(ncol(p)-3):ncol(p)])
   expcols<-c("Propability","lower","upper","dependent")
   expect_equal(pcols, expcols)
 
-  p <- predict_with_ci.orm(test_model_002, cran_rzs, Rural, sex)
+  p <- predict_with_ci(test_model_002, cran_rzs, Rural, sex)
   pcols<- colnames(p[(ncol(p)-3):ncol(p)])
   expect_equal(pcols, expcols)
 
 })
+
+
